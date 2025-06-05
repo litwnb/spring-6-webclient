e89330842f148a1753c2ddefb0055700b2801040
Kostiantyn Mamaisur <kmamaisur@gmail.com>
2025-06-05 14:50:59 +0300

    getting list as java map

--- src/main/java/com/litwnb/spring6webclient/client/BeerClient.java
+++ src/main/java/com/litwnb/spring6webclient/client/BeerClient.java
@@ -2,6 +2,10 @@ package com.litwnb.spring6webclient.client;
 
 import reactor.core.publisher.Flux;
 
+import java.util.Map;
+
 public interface BeerClient {
     Flux<String> listBeers();
+
+    Flux<Map> listBeersMap();
 }
--- src/test/java/com/litwnb/spring6webclient/client/BeerClientImplTest.java
+++ src/test/java/com/litwnb/spring6webclient/client/BeerClientImplTest.java
@@ -4,15 +4,36 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static org.awaitility.Awaitility.await;
+
 @SpringBootTest
 class BeerClientImplTest {
     @Autowired
     BeerClient client;
 
     @Test
-    void listBeers() throws InterruptedException {
-        client.listBeers().subscribe(System.out::println);
+    void testGetMap() {
+        AtomicBoolean atomicBoolean = new AtomicBoolean(false);
 
-        Thread.sleep(1000L);
+        client.listBeersMap().subscribe(response -> {
+            System.out.println(response);
+            atomicBoolean.set(true);
+        });
+
+        await().untilTrue(atomicBoolean);
+    }
+
+    @Test
+    void listBeers() {
+        AtomicBoolean atomicBoolean = new AtomicBoolean(false);
+
+        client.listBeers().subscribe(response -> {
+            System.out.println(response);
+            atomicBoolean.set(true);
+        });
+
+        await().untilTrue(atomicBoolean);
     }
 }
